fa1678fd7ab84dd0f43e17b59f849f7d5750ffdd
Ikhsan Fajar Nugroho <59926991+ikhsannugs@users.noreply.github.com>
2024-10-17 00:26:52 +0700

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -6,7 +6,7 @@ ENVIRONMENT = os.getenv('DJANGO_HOST', 'ec2-3-85-77-148.compute-1.amazonaws.com'
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = os.getenv ('DJANGO_HOST', 'localhost')
+ALLOWED_HOSTS = os.getenv('DJANGO_HOST', 'localhost')
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1','ec2-3-85-77-148.compute-1.amazonaws.com']
 
 INSTALLED_APPS = [
